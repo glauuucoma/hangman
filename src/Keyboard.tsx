794155a3d9b74ae810079fb2477173277c38e629
glauuucoma <kulebadavid@gmail.com>
2024-04-15 23:31:25 -0400

    complete project

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,3 +1,4 @@
+import { KeyboardEvent } from "react";
 import styles from "./Keyboard.module.css";
 
 const KEYS = [
@@ -29,7 +30,19 @@ const KEYS = [
   "z",
 ];
 
-export function Keyboard() {
+type KeyboardProps = {
+  activeLetters: string[];
+  inactiveLetters: string[];
+  disabled?: boolean;
+  addGuessedLetter: (letter: string) => void;
+};
+
+export function Keyboard({
+  activeLetters,
+  inactiveLetters,
+  disabled = false,
+  addGuessedLetter,
+}: KeyboardProps) {
   return (
     <div
       style={{
@@ -39,8 +52,16 @@ export function Keyboard() {
       }}
     >
       {KEYS.map((key) => {
+        const isActive = activeLetters.includes(key);
+        const isInactive = inactiveLetters.includes(key);
         return (
-          <button className={`${styles.btn}`} key={key}>
+          <button
+            onClick={() => addGuessedLetter(key)}
+            className={`${styles.btn} ${isActive ? styles.active : ""}
+            ${isInactive ? styles.inactive : ""} `}
+            disabled={isInactive || isActive || disabled}
+            key={key}
+          >
             {key}
           </button>
         );
